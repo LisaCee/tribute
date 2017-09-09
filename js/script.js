c83325634d32220ae4a06364b87f6d495114889b
Lisa Cee <three_nineteen@yahoo.com>
2017-09-09 14:57:36 -0700

    initial 2.0

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.carousel').slick({
+	autoplay: true,
+  dots: false,
+	arrows: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+	});
+});
